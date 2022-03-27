f0c6b912bbe1ee1730f7d6dff51f4ed258e493f3
dengwww <vinzry@163.com>
2022-03-27 23:01:56 +0800

    remove debug codes

--- src/MusicalScore/Graphical/VexFlow/VexFlowGraphicalNote.ts
+++ src/MusicalScore/Graphical/VexFlow/VexFlowGraphicalNote.ts
@@ -43,13 +43,6 @@ export class VexFlowGraphicalNote extends GraphicalNote {
                     const tmpData: any = { element: note_SVG, parent: parent };
                     osmd.renderingOptions.didRenderNoteSVG(tmpData);
                 }
-
-                note_SVG.addEventListener("click", (e) => {
-                    console.log("note_SVG ---> ", note_SVG);
-                    console.log("this ---> ", parent);
-                    console.log("asdfasdfasd");
-                    console.log("asdfasdfasd");
-                });
             }
         });
     }
