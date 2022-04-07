e7b14ac4e6968eeef612ad49c73939e9a4c759c4
dengwww <vinzry@163.com>
2022-04-07 22:05:16 +0800

    export custom options

--- src/OpenSheetMusicDisplay/index.ts
+++ src/OpenSheetMusicDisplay/index.ts
@@ -3,4 +3,8 @@
 export * from "./AJAX";
 export * from "./Cursor";
 export * from "./OSMDOptions";
+
+export * from "../Custom/NoteCursorOptions";
+export * from "../Custom/RenderingOptions";
+
 export * from "./OpenSheetMusicDisplay";
